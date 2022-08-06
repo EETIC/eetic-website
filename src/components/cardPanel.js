c4a09d904c6e1e9ff34bbc0752b3fd2e65dd7ead
JaVD054 <jeedu124@gmail.com>
2022-08-07 00:09:45 +0530

    cardpanel (initial)

--- src/components/cardPanel.js
+++ src/components/cardPanel.js
@@ -1,17 +1,24 @@
 import React from "react";
-import Card from './card';
+import EventCard from './card';
 import './css/cardPanel.css';
 import './css/card.css';
-function cardPanel(){
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
+const items =[
+  <EventCard/>,
+        <EventCard/>,
+        <EventCard/>,
+        <EventCard/>,
+        <EventCard/>,
+]
+function CardPanel(){
     return(
-    <div className="cardPanel">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+      <div className="p-4 ">
+        <h1 className="ms-2 font">Upcoming Event</h1>
+        <AliceCarousel mouseTracking items={items} responsive={{0: {items: 1,},512:{items:2},1024: {items: 3}}} />
       </div>
     );
 }
 
-export default cardPanel;
+export default CardPanel;
